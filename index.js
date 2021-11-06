095855a6779896b09dbd1512306117f34522eb8f
Afanasiev Dmitriy <afanasiev.dimon@outlook.com>
2021-11-06 21:49:21 +0200

    delete comments

--- index.js
+++ index.js
@@ -42,30 +42,3 @@ function invokeAction({ action, id, name, email, phone }) {
 }
 
 invokeAction(argv);
-
-// const argv = require("yargs").argv;
-
-// function invokeAction({ action, id, name, email, phone }) {
-//   switch (action) {
-//     case "list":
-//       listContacts();
-//       break;
-
-//     case "get":
-//       getContactById(id);
-//       break;
-
-//     case "add":
-//       addContact(name, email, phone);
-//       break;
-
-//     case "remove":
-//       removeContact(id);
-//       break;
-
-//     default:
-//       console.warn("\x1B[31m Unknown action type!");
-//   }
-// }
-
-// invokeAction(argv);
